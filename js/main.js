8efd3008a00fedfed284447e5f01362ee9e6bbd2
Yusuf Celik <302801376@student.rocmondriaan.nl>
2022-03-09 11:29:40 +0100

    Expert is klaar en zit in localstorage else van api

--- js/main.js
+++ js/main.js
@@ -1,48 +1,44 @@
 console.log("main.js loaded")
 
-const quotes = [
-  // Array met alle quotes
-  { 
-    quote: "My fault original gangster",
-    from: "-Not an original gangster"
-},
-// ^Object om quote en quote persoon op te slaan^
-{ 
-  quote: 'If life were predictable it would cease to be life, and be without flavor.',
-  from: '-Eleonor Roosevelt'
-},
-// ^Object om quote en quote persoon op te slaan^
-{ 
-  quote: 'Life is what happens when you\'re busy making other plans',
-  from: '-John Lennon'
-},
-// ^Object om quote en quote persoon op te slaan^
-{ 
-  quote: 'If life gives you lemons, <br> make lemonade',
-  from: '-Dunno - sometime...'
-},
-// ^Object om quote en quote persoon op te slaan^
-];
-
 const quoteSentence = document.querySelector(".quote");
 const quotePerson = document.querySelector(".quote-from");
 const quoteBtn = document.querySelector(".quote-btn");
-// Variabelen met quote, quote persoon en quote button
+// // ^Variabelen met quote, quote persoon en quote button^
+let quotes;
 
-quoteBtn.addEventListener("click", randomQuote)
-// als je op button klikt voert hij randomQuote function uit
-
-function randomQuote() {
-  const getQuoteNmr = Math.floor(Math.random() * quotes.length);
-  // Zorgt dat hij een nummer tussen de 0 en 1 pakt en * de lengte van de aantal
-  // quotes pakt en afrond omlaag zodat hij de laatste quote ook pakt.
-  const useQuote = quotes[getQuoteNmr];
-  // getQuoteNmr geeft een nummer en kiest 1 van de objecten
-  quoteSentence.innerHTML = useQuote.quote;
-  // Vult de quote van het gegeven object
-  quotePerson.innerHTML = useQuote.from;
-  // Vult de from van het gegeven object
+async function init(){
+  if(storedQuotes === null) {
+    // ^Check if localStorage key "quoteArray" is empty^
+  const res = await fetch("https://type.fit/api/quotes")
+  // ^Fetch API^
+  quotes = await res.json();
+  // If quoteArray is not empy use localstorage
+  localStorage.setItem("quoteArray", JSON.stringify(quotes))
+  } else {
+    quotes = storedQuotes;
+  }
+  getQuote();
 }
 
-randomQuote();
-// Zorgt dat er al een random quote in de div ligt 
+const storedQuotes = JSON.parse(localStorage.getItem("quoteArray"));
+
+function getQuote() {
+  const num = Math.floor(Math.random()*quotes.length);
+  // ^Pakt random nummer tussen 0 en quotes.length^
+  const item = quotes[num];
+  // ^Pakt de quote van een random nummer in lijst^
+  const quote = item.text;
+  // ^Pak de quote text^
+  const author = item.author;
+  // ^Pak de author text^
+  quoteSentence.innerText = quote;
+  // ^Zet text quoteSentence^
+  quotePerson.innerText = author;
+  // ^Zet text quotePerson^
+}
+
+quoteBtn.addEventListener("click", getQuote)
+// ^klik voor random quote^
+
+init();
+// ^zet alvast 1 random quote in plaats^
